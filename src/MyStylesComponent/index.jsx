5cb4253e4f741eb84c915d962824a713f37542d8
Constantin <constantin.veltri@ubisoft.com>
2019-02-08 10:36:33 -0500

    first commit

---
+++ src/MyStylesComponent/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+/* eslint-disable react/prefer-stateless-function */
+class MyStylesComponent extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+MyStylesComponent.propTypes = {};
+
+export default MyStylesComponent;
